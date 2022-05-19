8c12f51e93ce8345b82dfa8d5917be793aa9f552
MackInTech <94152169+EOSandCHILL@users.noreply.github.com>
2022-05-19 17:29:28 -0400

    Add files via upload

---
+++ JavaTree/src/Main.java
@@ -0,0 +1,20 @@
+// Define your tree class in this file!
+public class Main {
+    // class code goes here
+    public static void main(String[] args) {
+        // code for main method goes here
+        System.out.println("Hey there, I’m MackInTech and I’m learning to code in Java!");
+        System.out.println("I’m going to plant a tree for you today, so I'm prepared to get my hands dirty!");
+        System.out.println("Here's the tree I planted for you:");
+        System.out.println(" ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("********* ");
+        System.out.println("  ***** ");
+        System.out.println("    *  ");
+        System.out.println("    *  ");
+        System.out.println("~~~~*~~~~");
+
+        /* Hello! I really hope you enjoy the Java tree I've planted for you!*/
+    }
+}
